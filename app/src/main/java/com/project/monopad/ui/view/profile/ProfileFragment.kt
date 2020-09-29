771be1fb220d126940bb28650fd4287e5154dd42
Haeseong5 <dhgotjd1234@naver.com>
2020-09-30 00:06:35 +0900

    #7 - MainActivity  - BottomNavigationView 구현
    - 각 Fragment 클래스 생성

---
+++ app/src/main/java/com/project/monopad/ui/view/profile/ProfileFragment.kt
@@ -0,0 +1,29 @@
+package com.project.monopad.ui.view.profile
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.monopad.R
+import com.project.monopad.databinding.ActivityMainBinding
+import com.project.monopad.ui.base.BaseFragment
+import com.project.monopad.ui.viewmodel.MovieViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class ProfileFragment : BaseFragment<ActivityMainBinding, MovieViewModel>() {
+
+    override val viewModel: MovieViewModel by viewModel()
+
+    override val layoutResourceId: Int
+        get() = R.layout.fragment_profile
+
+
+    override fun initStartView() {
+    }
+
+    override fun initDataBinding() {
+    }
+
+    override fun initAfterBinding() {
+    }
+}
