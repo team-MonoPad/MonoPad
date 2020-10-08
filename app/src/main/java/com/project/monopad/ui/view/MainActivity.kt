6a254af3f9cf821a40da5311fd9522b532015280
DevHyeonseong <ykiw911@naver.com>
2020-10-09 04:29:46 +0900

    #37 - test code delete

--- app/src/main/java/com/project/monopad/ui/view/MainActivity.kt
+++ app/src/main/java/com/project/monopad/ui/view/MainActivity.kt
@@ -40,7 +40,6 @@ class MainActivity : BaseActivity<ActivityMainBinding, MovieViewModel>() {
     }
 
     override fun initAfterBinding() {
-        startActivity(Intent(this, DetailActivity::class.java))
         //observing & add item to adapter
     }
 
