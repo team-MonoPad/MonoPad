8ab2f9de39d966c2125ef444107962617e5bad3f
JeongJae-yun <wodbs135@gmail.com>
2020-10-21 21:16:00 +0900

    update - Image download using Glide library - data convert Date to Long - save data into room

--- app/src/main/java/com/project/monopad/network/local/database/DiaryDatabase.kt
+++ app/src/main/java/com/project/monopad/network/local/database/DiaryDatabase.kt
@@ -6,11 +6,12 @@ import androidx.room.TypeConverters
 import com.project.monopad.model.entity.Movie
 import com.project.monopad.model.entity.Review
 import com.project.monopad.model.network.dto.Genre
+import com.project.monopad.network.local.converter.DateConverter
 import com.project.monopad.network.local.converter.GenreConverter
 import com.project.monopad.network.local.converter.MovieConverter
 
 @Database(entities = [Review::class, Movie::class, Genre::class], version = 2, exportSchema = false)
-@TypeConverters(*[GenreConverter::class, MovieConverter::class])
+@TypeConverters(*[GenreConverter::class, MovieConverter::class, DateConverter::class])
 abstract class DiaryDatabase : RoomDatabase(){
 
     abstract fun diaryDao() : DiaryDao
