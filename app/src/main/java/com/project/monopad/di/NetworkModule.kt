8f385ed30872b339db7f8f96fabcfb46078424b0
JeongJae-yun <wodbs135@gmail.com>
2020-10-30 23:56:57 +0900

    network error state check

--- app/src/main/java/com/project/monopad/di/NetworkModule.kt
+++ app/src/main/java/com/project/monopad/di/NetworkModule.kt
@@ -2,25 +2,32 @@ package com.project.monopad.di
 
 import com.project.monopad.network.remote.api.MovieRepoApi
 import com.project.monopad.network.remote.api.UserApiClient
-import okhttp3.OkHttpClient
+import com.project.monopad.exception.NoConnectionInterceptor
+import okhttp3.*
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val networkModule = module{
     single<MovieRepoApi>{
         Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
+            .client(get(named("network")))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(MovieRepoApi::class.java)
     }
 
-    single{
-        val b = OkHttpClient.Builder()  // 이 클라이언트를 통해 오고 가는 네트워크 요청/응답을 로그로 표시하도록 합니다.
-        b.build()
+    single(named("network")){
+        OkHttpClient.Builder().apply {
+            addInterceptor(NoConnectionInterceptor(get()))
+                .connectTimeout(1,TimeUnit.MINUTES)
+                .readTimeout(30,TimeUnit.SECONDS)
+        }.build()
     }
 
     single{
